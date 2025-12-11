75b052f30daf4d8c22607c96be633aa37080d957
keeenzay <mkujava@wisc.edu>
2025-12-10 20:12:24 -0600

    :(

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/p158/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
